da21635486e39eef16a20525b38edfe9b2ba0760
Ana <anavd12@gmail.com>
2022-05-18 19:09:56 -0300

    with agregarStock

--- UT4TA9/src/main/java/ucu/edu/uy/ta92022/Almacen.java
+++ UT4TA9/src/main/java/ucu/edu/uy/ta92022/Almacen.java
@@ -30,7 +30,20 @@ public class Almacen implements IAlmacen {
 
     @Override
     public Boolean agregarStock(Comparable clave, Integer cantidad) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(productos.buscar(clave) != null){
+            IElementoAB<Producto> elem = productos.buscar(clave);  //buscamos producto el arbol de productos usando clave
+            Producto dato = elem.getDatos();     // del Nodo encontrado en el arbol de prodcutos, obtenemos el Dato que es el Producto
+            int stockActual = dato.getStock();    // obtenemos el stock al momento de ese Producto
+            dato.setStock(stockActual + cantidad);     // sumamos stock
+            return true;
+        }else{
+            //Si el producto no existe en el arbol de Productos, lo agregamos con su stock
+            Producto prod = new Producto(clave, (String) clave);
+            prod.setStock(cantidad);
+            IElementoAB aux = new TElementoAB(clave, prod);
+            productos.insertar(aux);
+            return false;
+        }
     }
 
     @Override
