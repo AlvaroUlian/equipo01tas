643635a26505ac7e7aac2da57f5a10612abd43c3
facundomartinez93 <facundo.martinez@correo.ucu.edu.uy>
2022-07-07 19:21:49 -0300

    UT5_TA_FINALSEMESTRE

---
+++ UT5_TA_FINALSEMESTRE/src/main/java/com/mycompany/ut5_ta_finalsemestre/UT5_TA_FINALSEMESTRE.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ut5_ta_finalsemestre;
+
+/**
+ *
+ * @author facum
+ */
+public class UT5_TA_FINALSEMESTRE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
